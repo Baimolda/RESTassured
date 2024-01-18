812c3253533dc768404ca534a2895fbf8a9ce8f3
Admin <asetbaimolda@gmail.com>
2024-01-18 22:16:31 +0600

    showLinkRedirect

--- src/main/java/HelloWorldTest.java
+++ src/main/java/HelloWorldTest.java
@@ -1,22 +1,30 @@
 import io.restassured.RestAssured;
+import io.restassured.http.Headers;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class HelloWorldTest {
 
     @Test
     public void testRestAssured() {
+        Map<String,String> headers = new HashMap<>();
+        headers.put("myHeader1","myValue1");
+        headers.put("myHeader2", "myValue2");
 
         Response response = RestAssured
                 .given()
                 .redirects()
-                .follow(true)
+                .follow(false)
                 .when()
                 .get("https://playground.learnqa.ru/api/get_303")
                 .andReturn();
+        response.prettyPrint();
 
-        int statusCode = response.statusCode();
-        System.out.println(statusCode);
+        String locationHeader = response.getHeader("Location");
+        System.out.println(locationHeader);
     }
 }
