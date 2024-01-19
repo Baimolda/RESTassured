91a7030507195cdab9f95f79694750f228a61ce0
Admin <asetbaimolda@gmail.com>
2024-01-19 22:26:35 +0600

    createUser

--- src/main/java/lib/Assertions.java
+++ src/main/java/lib/Assertions.java
@@ -9,13 +9,27 @@ public class Assertions {
     public static void asserJsonByName(Response responseCheckAuth, String userId, int userIdOnAuth) {
     }
 
-    public void assertJsonByName(Response Response, String name, int expectedValue){
+    public void assertJsonByName(Response Response, String name, int expectedValue) {
         Response.then().assertThat().body("$", hasKey(name));
 
         int value = Response.jsonPath().getInt(name);
         assertEquals(expectedValue, value, "JSON value is not equal to expected value");
-
-
     }
 
+    public static void assertResponseTextEquals(Response Response, String expectedAnswer) {
+        assertEquals(
+                expectedAnswer,
+                Response.asString(),
+                "Response text is not as expected"
+        );
+    }
+
+    public static void assertResponseCodeEquals(Response Response, int expectedStatusCode) {
+        assertEquals(
+                expectedStatusCode,
+                Response.statusCode(),
+                "Response status code is not as expected"
+        );
+
+    }
 }
