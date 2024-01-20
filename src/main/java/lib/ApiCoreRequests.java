d77c63c973a418ab6adf56934088a685e76dcf91
Admin <asetbaimolda@gmail.com>
2024-01-20 19:23:28 +0600

    addAllureTegsAllTests

---
+++ src/main/java/lib/ApiCoreRequests.java
@@ -0,0 +1,49 @@
+package lib;
+
+import io.qameta.allure.Step;
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.http.Header;
+import io.restassured.response.Response;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+public class ApiCoreRequests {
+    @Step("Делаем GET-request с koen и авторизац куки")
+    public Response makeGetRequest(String url, String token, String cookie) {
+        return given()
+                .filter(new AllureRestAssured())
+                .header(new Header("x-csrf-token", token))
+                .cookie("auth_sid", cookie)
+                .get(url)
+                .andReturn();
+    }
+
+    @Step("Делаем GET-request только с авторизационной cookie")
+    public Response makeGetRequestWithCookie(String url, String cookie) {
+        return given()
+                .filter(new AllureRestAssured())
+                .cookie("auth_sid", cookie)
+                .get(url)
+                .andReturn();
+    }
+
+    @Step("Делаем GET-request только с авторизационной token")
+    public Response makeGetRequestWithToken(String url, String token) {
+        return given()
+                .filter(new AllureRestAssured())
+                .header(new Header("x-csrf-token", token))
+                .get(url)
+                .andReturn();
+    }
+
+    @Step("Делаем POST-request ")
+    public Response makePostRequest(String url, Map<String, String> authData) {
+        return given()
+                .filter(new AllureRestAssured())
+                .body(authData)
+                .post(url)
+                .andReturn();
+    }
+}
