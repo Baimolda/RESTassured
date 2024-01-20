c267a40701a3dffaa217b0c738284a6aed5d41e9
Admin <asetbaimolda@gmail.com>
2024-01-20 17:19:49 +0600

    justCommitAddChange

--- src/main/java/lib/Assertions.java
+++ src/main/java/lib/Assertions.java
@@ -6,11 +6,17 @@ import static org.hamcrest.Matchers.hasKey;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Assertions {
-
     public static void assertJsonByName(Response Response, String name, int expectedValue) {
         Response.then().assertThat().body("$", hasKey(name));
 
         int value = Response.jsonPath().getInt(name);
+        assertEquals(expectedValue, value, "JSON value is not equal to expected value");
+    }
+
+    public static void assertJsonByName(Response Response, String name, String expectedValue) {
+        Response.then().assertThat().body("$", hasKey(name));
+
+        String value = Response.jsonPath().getString(name);
         assertEquals(expectedValue, value, "JSON value is not equal to expected value");
     }
 
